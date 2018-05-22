10ba1c935ff510cbfc019f42369d5a0c1daab963
Daniel Staněk <daniel.stanek@techlib.cz>
2018-05-22 11:14:16 +0200

    Fix missed rebase.
    Signed-off-by: Daniel Staněk <daniel.stanek@techlib.cz>

--- ckanext/datastore/logic/action.py
+++ ckanext/datastore/logic/action.py
@@ -526,7 +526,6 @@ def datastore_search_sql(context, data_dict):
 
 
 def set_datastore_active_flag(model, data_dict, flag):
-<<<<<<< HEAD
     '''
     Set appropriate datastore_active flag on CKAN resource.
 
@@ -535,8 +534,6 @@ def set_datastore_active_flag(model, data_dict, flag):
     # We're modifying the resource extra directly here to avoid a
     # race condition, see issue #3245 for details and plan for a
     # better fix
-=======
->>>>>>> 8882377c6... Fix datastore_active race condition also for resource deletion
     update_dict = {'datastore_active': flag}
 
     # get extras(for entity update) and package_id(for search index update)
